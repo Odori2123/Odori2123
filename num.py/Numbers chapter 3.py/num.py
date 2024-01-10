6b60b977a92d4e5e11caff301a99715518a48e95
Odori2123 <kennyodori@gmail.com>
2024-01-09 19:04:21 -0500

    num.py

---
+++ num.py/Numbers chapter 3.py/num.py
@@ -0,0 +1,66 @@
+# ''' Question 1
+# Write a program that generates and prints 50 random integers, 
+# each between 3 and 6.
+
+# '''
+
+# from random import randint
+# x = randint(1,50)
+# print( "A random number between 3 and 6:",x) 
+
+# '''Question 2
+# Write a program that generates a random number, x , between 1 and 50, a random number y
+# between 2 and 5, and computes x y .
+# '''
+
+# from random import randint
+# x= randint(1,50)
+# y= randint(2,5)
+# print ("A random number between 1 and 50;",x)
+# print ("A random number between 2 and 5;",y)
+# print (x^y)
+
+
+# ''' Question 5
+# Write a program that generates 50 random numbers such that the first number is between 1 and 2, the second is between 1 and 3, the third is between 1 and 4, . . . , and the last is between
+# 1 and 51.
+# '''
+
+# from random import randint
+# x= randint(1,50)
+# y= randint(1,50)
+# z= randint(1,50)
+# s= randint(1,50)
+
+# print("A random number between 1 and 2;",x)
+# print("A random number between 1 and 3;",y)
+# print("A random number between 1 and 4;",z)
+# print("A random number between 1 and 51;",s)
+
+
+# '''Question 6
+# Write a program that asks the user to enter two numbers, x and y , and computes |x− y|/x+ y .
+# '''
+
+
+# x= eval(input('Enter value for x '))
+# y=eval(input('Enter value for y '))
+# s=(x-y)/(x+y)
+# print(s)
+
+
+# '''Question 3
+#  Write a program that generates a random number between 1 and 10 and prints your name
+#  that many times.
+#  '''
+
+ 
+# from random import randint
+# x= randint(1,10)
+# print("A random number between 1 and 10;",x)
+# for i in range(7): 
+#     print(i, 'Shalom')
+
+
+
+
